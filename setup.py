b76b13beb5e70691aec1c0e1b9213c68c5416f67
Samuel Dantas <dantasamuca@gmail.com>
2020-12-16 00:52:07 -0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='payload_translator',
+      version='2.0.0',
+      description='A simple dict translator',
+      author='Samuel Dantas',
+      author_emai='dantasamuca@gmail.com',
+      packages=find_packages())
