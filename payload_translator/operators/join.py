e148bfd1b6a36ccedaca41abb94932032fc81f29
Samuel Dantas <dantasamuca@gmail.com>
2021-01-18 00:06:16 -0300

    Add sum operator

--- payload_translator/operators/join.py
+++ payload_translator/operators/join.py
@@ -1,4 +1,10 @@
 class Join:
+    """Operator to join values got from another operators. It join them into a
+    string with the values obtained.
+    :param: operators: the operators object to get the values
+    :param: delimiter: the delimiter character to put in the final string with
+    the joined values
+    """
     def __init__(self, *operators, delimiter=' '):
         self.operators = operators
         self.delimiter = delimiter
